db4be4f61a0e477c524419271c38b34e12f65b99
kimyerimweb <kimyerimweb@gmail.com>
2022-04-07 12:01:34 +0900

    🚧 Seperate user-gallery mobile/desktop

--- carousels.js
+++ carousels.js
@@ -11,9 +11,9 @@ const productCarousel = tns({
   preventScrollOnTouch: true,
 })
 
-const galleryCarousel = tns({
-  container: '.user-gallery .slider-list',
-  navContainer: '.user-gallery .thumbnail-list',
+const galleryCarouselMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
   navAsThumbnails: true,
   gutter: 4,
   edgePadding: 16,
@@ -23,11 +23,18 @@ const galleryCarousel = tns({
   mouseDrag: true,
   arrowKeys: true,
   preventScrollOnTouch: true,
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    },
-  },
+})
+
+const galleryCarouselDesktop = tns({
+  container: '.user-gallery.is-desk .slider-list',
+  navContainer: '.user-gallery.is-desk .thumbnail-list',
+  navAsThumbnails: true,
+  gutter: 6,
+  edgePadding: 75,
+  loop: false,
+  controls: true,
+  controlsContainer: '.user-gallery-controls',
+  mouseDrag: true,
+  arrowKeys: true,
+  preventScrollOnTouch: true,
 })
