b7ba14d39573e182b311cb9cff86d42356234bc4
kimyerimweb <kimyerimweb@gmail.com>
2022-04-11 07:16:37 +0900

    🚧 Detect position of tab-panel

--- javascript/productTab.js
+++ javascript/productTab.js
@@ -3,6 +3,21 @@ const productTabButtonList = productTab.querySelectorAll('button')
 
 let currentActiveTab = productTab.querySelector('.is-active')
 
+const productTabPanelIdList = [
+  'product-spec',
+  'product-review',
+  'product-inquiry',
+  'product-shipment',
+  'product-recommendation',
+]
+
+const productTabPanelList = productTabPanelIdList.map((panelId) => {
+  const tabPanel = document.querySelector(`#${panelId}`)
+  return tabPanel
+})
+
+const productTabPanelPositionMap = {}
+
 function toggleActiveTab() {
   const tabItem = this.parentNode
 
@@ -13,9 +28,9 @@ function toggleActiveTab() {
   currentActiveTab = tabItem
 }
 
-function scrollToTabPannel() {
-  const tabPannelId = this.parentNode.getAttribute('aria-labelledby')
-  const tabPannel = document.querySelector(`#${tabPannelId}`)
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
 
   const gnbHeight = document
     .querySelector('.gnb')
@@ -24,15 +39,26 @@ function scrollToTabPannel() {
     .querySelector('.lnb')
     .getBoundingClientRect().height
   const productTabHeight = productTab.getBoundingClientRect().height
-  const tabPannelTop = tabPannel.getBoundingClientRect().top
+  const tabPanelTop = tabPanel.getBoundingClientRect().top
 
   window.scrollBy({
-    top: tabPannelTop - gnbHeight - lnbHeigth - productTabHeight,
+    top: tabPanelTop - gnbHeight - lnbHeigth - productTabHeight,
     behavior: 'smooth',
+  })
+}
+
+function detectTabPannelPosition() {
+  productTabPanelList.forEach((panel) => {
+    const id = panel.getAttribute('id')
+    const position = window.scrollY + panel.getBoundingClientRect().top
+    productTabPanelPositionMap[id] = position
   })
 }
 
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
-  button.addEventListener('click', scrollToTabPannel)
+  button.addEventListener('click', scrollToTabPanel)
 })
+
+window.addEventListener('load', detectTabPannelPosition)
+window.addEventListener('resize', detectTabPannelPosition)
