b7cedb994fe9d00f4e23ce515070a4b0c021e5c1
kimyerimweb <kimyerimweb@gmail.com>
2022-04-11 06:17:31 +0900

    💫 Scroll to tab-panel when product-tab-item is clicked

--- javascript/productTab.js
+++ javascript/productTab.js
@@ -13,6 +13,26 @@ function toggleActiveTab() {
   currentActiveTab = tabItem
 }
 
+function scrollToTabPannel() {
+  const tabPannelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPannel = document.querySelector(`#${tabPannelId}`)
+
+  const gnbHeight = document
+    .querySelector('.gnb')
+    .getBoundingClientRect().height
+  const lnbHeigth = document
+    .querySelector('.lnb')
+    .getBoundingClientRect().height
+  const productTabHeight = productTab.getBoundingClientRect().height
+  const tabPannelTop = tabPannel.getBoundingClientRect().top
+
+  window.scrollBy({
+    top: tabPannelTop - gnbHeight - lnbHeigth - productTabHeight,
+    behavior: 'smooth',
+  })
+}
+
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollToTabPannel)
 })
